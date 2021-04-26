5677b8d3e4836d736d8eb3b30e1fe74b8ca3f6b7
othmanehann <rihera3025@mailnet.top>
2021-04-26 15:57:19 +0000

    add total

--- DataFood.js
+++ DataFood.js
@@ -3,19 +3,19 @@ export const DataFood = [
     {
         id: '0001',
         name: "Food1",
-        price: "20",
+        price: 40,
         image: require("./assets/img1.jpg")
     },
     {
         id: '0002',
         name: "Food2",
-        price: "30",
+        price: 30,
         image: require("./assets/img2.jpg")
     },
     {
         id: '0003',
         name: "Food3",
-        price: "35",
+        price: 20,
         image: require("./assets/img4.jpg")
     },
 ]
